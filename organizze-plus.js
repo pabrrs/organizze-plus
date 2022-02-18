51a3b4924b641f23100185d174ba29e02c548bac
pabrrs <paulo.alfredo.barros@gmail.com>
2022-02-17 20:17:31 -0400

    feat: Add ordering by account

--- organizze-plus.js
+++ organizze-plus.js
@@ -7,7 +7,7 @@ function checkDocument() {
     if (accountsDiv) {
         console.log('organizze-plus :: doc is ready!')
         
-        orderAccountsByBalanceDesc();
+        orderAccountsByAccountType();
     
         var toggleInput = document.getElementsByClassName('zze-config ng-binding ng-scope')[0].firstChild;
         toggleInput.addEventListener('click', toggleInvestmentAccounts);
@@ -42,6 +42,45 @@ function orderAccountsByBalanceDesc() {
     })
     accountsUl.outerHTML = '<ul>' + accountsOrdered.reduce(function (init, el) { return init + el.outerHTML }, '') + '</ul>'
 };
+
+function orderAccountsByAccountType() {
+    var accountsUl = document.querySelector('.zze-list-accounts > ul');
+    var accountsList = accountsUl.childNodes;
+    var accounts = [];
+    
+    accountsList.forEach(function (el) {
+        if (el.nodeType !== Node.COMMENT_NODE) {
+            accounts.push(el)
+        };
+    });
+    
+    var accountsOrdered = accounts.sort(function (a, b) {
+        var aAccountImg = a.querySelector('a > div > img');
+        var aAccountIcon = a.querySelector('a > div > i');
+        
+        if (aAccountIcon) {
+            aAccountIcon = aAccountIcon.parentNode;
+        }
+        
+        var bAccountImg = b.querySelector('a > div > img');
+        var bAccountIcon = b.querySelector('a > div > i');
+
+        if (bAccountIcon) {
+            bAccountIcon = bAccountIcon.parentNode;
+        }
+
+        var aLogo = aAccountImg ? aAccountImg.src.split('/').pop() : aAccountIcon.style;
+        var bLogo = bAccountImg ? bAccountImg.src.split('/').pop() : bAccountIcon.style;
+
+        if (aLogo > bLogo)
+            return 1;
+        else 
+            return -1;
+    });
+
+    accountsUl.outerHTML = '<ul>' + accountsOrdered.reduce(function (init, el) { return init + el.outerHTML }, '') + '</ul>'
+};
+
 
 function toggleInvestmentAccounts() {
     var accountsListItems = document.querySelectorAll('.zze-list-accounts > ul > li');
